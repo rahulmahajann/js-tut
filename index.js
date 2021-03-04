f6da9d613e7d9dd694c246382403641d8be37fb0
rahul1237 <rahulmahajan98645@gmail.com>
2021-03-04 21:44:13 +0530

    js added

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-console.log('hello world');
-console.error('this is an error');
-console.warn('this is a warning');
-click.innerHTML = '<em>this is rahul mahajan</em>'
+// console.log('hello world');
+// console.error('this is an error');
+// console.warn('this is a warning');
+// click.innerHTML = '<b>this is rahul mahajan</b>'
+
+// to clear the console!
+console.clear()
+
+let date = new Date()
+// console.log(date.getDate())
+// console.log(date.getDay());
+// console.log(date.getHours());
+// console.log(date.getSeconds());
+
+
+var arr = [1,2,3,4,'rahul mahajan']
+// console.log(arr[0]);
+
+// for(i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+
+
+
+// DOM MANIPULATION
+
