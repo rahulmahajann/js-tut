c5cae0543eca3150c95fe808dd85e0b0c9b7e600
rahul1237 <rahulmahajan98645@gmail.com>
2021-03-04 21:45:11 +0530

    js added

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-// console.log('hello world');
-// console.error('this is an error');
-// console.warn('this is a warning');
-// click.innerHTML = '<b>this is rahul mahajan</b>'
+console.log('hello world');
+console.error('this is an error');
+console.warn('this is a warning');
+click.innerHTML = '<b>this is rahul mahajan</b>'
 
 // to clear the console!
 console.clear()
 
 let date = new Date()
-// console.log(date.getDate())
-// console.log(date.getDay());
-// console.log(date.getHours());
-// console.log(date.getSeconds());
+console.log(date.getDate())
+console.log(date.getDay());
+console.log(date.getHours());
+console.log(date.getSeconds());
 
 
 var arr = [1,2,3,4,'rahul mahajan']
-// console.log(arr[0]);
+console.log(arr[0]);
 
-// for(i=0;i<arr.length;i++){
-//     console.log(arr[i]);
-// }
+for(i=0;i<arr.length;i++){
+    console.log(arr[i]);
+}
 
 
 
